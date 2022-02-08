ab3eb00e3f37d4ea4c60513d2e3c64409bdf003f
dlperezmartinez <dlperezmartinez@gmail.com>
2022-02-08 22:16:27 +0100

    Ejercicio 1 TERMINADO

--- src/UD4/Ejercicio1/AdressTester.java
+++ src/UD4/Ejercicio1/AdressTester.java
@@ -7,22 +7,41 @@ import java.util.Scanner;
 public class AdressTester {
     public static void main(String[] args) throws UnknownHostException {
         Scanner in = new Scanner(System.in);
-        boolean continuar = true;
 
         System.out.println("\n-ADRESS TESTER-\n");
         System.out.println("Introduce una dirección IP o un nombre de DOMINIO (\"exit\" o \"quit\" para salir)");
+        System.out.print("-> ");
 
-        while (continuar) {
-            System.out.print("->");
-            testAdress(in.nextLine());
-        }
+        while (testAdress(in.nextLine()));
     }
 
-    static void testAdress(String s) {
-        if (s.matches("[a-z._%+-]*")) {
-            System.out.println("funciona");
-        } else {
+    //Método que comprueba si "String s" es una dirección IP o si es una URL y devuelve respectivamente la opuesta
+    static boolean testAdress(String s) throws UnknownHostException {
+        if (!s.equals("exit") && !s.equals("quit")) {
+            InetAddress address = InetAddress.getByName(s);
 
+            if (address.getHostName().equals(s)) {
+                System.out.print("Adress: " + address.getHostAddress() + "\n");
+            }
+
+            if (address.getHostAddress().equals(s)) {
+                System.out.print("Servidor: " + address.getHostName() + "\n");
+            }
+
+        } else {
+            return false;
         }
+
+        System.out.print("\n-> ");
+
+        return true;
+
+//        if (s.matches("[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}")) {
+//            System.out.println(InetAddress.getByName(s).getHostName());
+//        } else if (s.matches("[a-z._%+-]*")) {
+//            System.out.println(InetAddress.getByName(s).getHostAddress());
+//        } else {
+//            System.out.println("\nDirección no válida.\n");
+//        }
     }
 }
