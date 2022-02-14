600dd5161c98291c8646ed8152afc9c4c1bc8e65
nocend <danmar.alu@iesbenigaslo.es>
2022-02-14 10:48:51 +0100

    Ejercicio 2 CONTINUADO

--- src/UD4/Ejercicio2/UrlTester.java
+++ src/UD4/Ejercicio2/UrlTester.java
@@ -1,34 +1,39 @@
 package UD4.Ejercicio2;
 
-import java.net.InetAddress;
-import java.net.URL;
-import java.net.UnknownHostException;
+import java.io.IOException;
+import java.net.*;
 import java.util.Scanner;
 
 public class UrlTester {
-    public static void main(String[] args) throws UnknownHostException {
+    public static void main(String[] args) throws UnknownHostException, MalformedURLException {
         Scanner in = new Scanner(System.in);
 
-        System.out.println("\n-ADRESS TESTER-\n");
-        System.out.println("Introduce una dirección IP o un nombre de DOMINIO (\"exit\" o \"quit\" para salir)");
+        System.out.println("\n-URL TESTER-\n");
+        System.out.println("Introduce una dirección URL (\"exit\" o \"quit\" para salir)");
         System.out.print("-> ");
 
         while (test(in.nextLine()));
     }
 
     //Método que comprueba si "String s" es una dirección IP o si es una URL y devuelve respectivamente la opuesta
-    static boolean test(String s) throws UnknownHostException {
+    static boolean test(String s) throws MalformedURLException {
         if (!s.equals("exit") && !s.equals("quit")) {
-            URL url;
 
-//            if (address.getHostName().equals(s)) {
-//                System.out.print("Adress: " + address.getHostAddress() + "\n");
-//            }
-//
-//            if (address.getHostAddress().equals(s)) {
-//                System.out.print("Servidor: " + address.getHostName() + "\n");
-//            }
+            URL url = new URL(s);
+            URLConnection urlConnection = new URLConnection(s) {
+                @Override
+                public void connect() throws IOException {
 
+                }
+            };
+            urlConnection.getContentType();
+
+            System.out.println( "\nProtocolo: " + url.getProtocol() +
+                                "\nAutoridad: " + url.getAuthority() +
+                                "\nPath: " + url.getPath() +
+                                "\nQuery: " + url.getQuery() +
+                                "\nFile: " + url.getFile() +
+                                "\nPort: " + url.getPort());
         } else {
             return false;
         }
